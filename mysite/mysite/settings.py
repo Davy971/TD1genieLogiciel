29613cf665802559ad644d8042774722d0c047e6
Davy971 <regalade.davy@gmail.com>
2018-10-04 00:02:28 -0400

    creation des tables

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'ifap.apps.IfapConfig'
+    'ifap.apps.IfapConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
